7af680010992ef7274bf0992a37b84c9e6ed1b66
RahulDubey391 <100185371+RahulDubey391@users.noreply.github.com>
2022-07-25 23:03:43 +0530

    multiprocessing code addition

--- src/controller/requestControl.py
+++ src/controller/requestControl.py
@@ -1,29 +1,39 @@
 from ..utilities import SnowflakeConnector
 import pandas as pd
+from multiprocessing import Process, Pool
 
 class DataController:
     def __init__(self,database,schema,table):
         self.database = database
         self.schema = schema
         self.table = table
-        self.sflkConnector = SnowflakeConnector()
 
+    def requestExecutor(self,query,filename):
+        con = SnowflakeConnector()
+        cur = con.get_con().cursor()
+        df = cur.execute(query).fetch_pandas_all()
+        self.fileWriter(df,filename)
+        return 'Done'
+    
     def pullQueries(self):
         querylist = []
-        query = '''SELECT * FROM %s.%s.%s;'''%(self.database,self.schema,self.table)
-        querylist.append(query)
+        q1 = '''SELECT * FROM %s.%s.%s;'''%(self.database,self.schema,self.table)
+        q2 = '''SELECT * FROM %s.%s.%s;'''%(self.database,self.schema,self.table)
+        querylist.append([q1,'1'])
+        querylist.append([q2,'2'])
         return querylist
     
     def fileWriter(self,df,filename):
         df.to_csv(filename,index=False,header=True)
     
     def leecher(self):
-        con = self.sflkConnector.get_con()
         queries = self.pullQueries()
-        for i in queries:
-            df = con.cursor().execute(i).fetch_pandas_all()
-            filename = 'samplefile.csv'
-            self.fileWriter(df,filename)
+        print(queries)
+
+        pool = Pool(processes=5)
+        results = pool.starmap(self.requestExecutor, queries)
+        for result in results:
+            print(result)
 
     def poster(self):
         return None
